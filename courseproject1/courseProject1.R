eadf662414888dc64c84d4cb9032c36e8d68e0e7
Brandon Wolfgang <brandonwolfgang@gmail.com>
2015-11-09 09:34:12 -0500

    updates to data directory to prevent file I/O errors

--- courseproject1/courseProject1.R
+++ courseproject1/courseProject1.R
@@ -1,15 +1,16 @@
-setwd('/Users/Brandon/dev/coursera/data-science/courses/04 - Exploratory Data Analysis/workspace')
-downloadUrl <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
-downloadFile <- './data/household_power_consumption.zip'
-downloadFile
-householdFile <- './data/household_power_consumption.txt'
-householdFile
+# Initialize data related variables
+dataUrl <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+dataZipped <- tempfile()
+dataPath <- paste(getwd(), 'data', sep = '/')
+dir.create(file.path(dataPath), showWarnings = FALSE)
 
-download.file(downloadUrl, downloadFile, method = 'curl')
-unzip(downloadFile, overwrite = T, exdir = tempdir())
+# Download data to tempfile() and unzip to working directory
+download.file(dataUrl, dataZipped, method = 'curl')
+unzip(zipfile = dataZipped, exdir = dataPath)
+householdFile <- paste(dataPath, 'household_power_consumption.txt', sep = '/')
 
+# Get data from file into plottable structure 
 plotData <- read.table(householdFile, header = T, sep = ';', na.strings = '?')
-
 finalData <- plotData[plotData$Date %in% c('1/2/2007', '2/2/2007'),]
 setTime <- strptime(paste(finalData$Date, finalData$Time, sep = ' '), '%d/%m/%Y %H:%M:%S')
 finalData <- cbind(setTime, finalData)
@@ -18,12 +19,12 @@ finalData <- cbind(setTime, finalData)
 hist(finalData$Global_active_power, col = 'red', main = 'Global Active Power', xlab = 'Global Active Power (kilowatts)')
 
 # Generating plot 2
-plot(finalData$setTime, finalData$Global_active_power, type = '1', col = 'black', xlab = '', ylab = 'Global Active Power (kilowatts)')
+plot(finalData$setTime, finalData$Global_active_power, type = 'l', col = 'black', xlab = '', ylab = 'Global Active Power (kilowatts)')
 
 # Generating plot 3
 columnLines <- c('black', 'red', 'blue')
 labels <- c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3')
-plot(finalData$setTime, finalData$Sub_metering_1, type = '1', col = columnLines[1], xlab = '', ylab = 'Energy sub metering')
+plot(finalData$setTime, finalData$Sub_metering_1, type = 'l', col = columnLines[1], xlab = '', ylab = 'Energy sub metering')
 lines(finalData$setTime, finalData$Sub_metering_2, col = columnLines[2])
 lines(finalData$setTime, finalData$Sub_metering_3, col = columnLines[3])
 legend('topright', legend = labels, col = columnLines, lty = 'solid')
@@ -32,11 +33,10 @@ legend('topright', legend = labels, col = columnLines, lty = 'solid')
 labels <- c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3')
 columnLines <- c('black', 'red', 'blue')
 par(mfrow = c(2, 2))
-plot(finalData$setTime, finalData$Global_active_power, type = '1', col = 'green', xlab = '', ylab = 'Global Active Power')
-plot(finalData$setTime, finalData$Voltage, type = '1', col = 'orange', xlab = 'datetime', ylab = 'Voltage')
-plot(finalData$setTime, finalData$Sub_metering_1, type = '1', xlab = '', ylab = 'Energy sub metering')
-lines(finalData$setTime, finalData$Sub_metering_2, type = '1', col = 'red')
-lines(finalData$setTime, finalData$Sub_metering_3, type = '1', col = 'blue')
+plot(finalData$setTime, finalData$Global_active_power, type = 'l', col = 'green', xlab = '', ylab = 'Global Active Power')
+plot(finalData$setTime, finalData$Voltage, type = 'l', col = 'orange', xlab = 'datetime', ylab = 'Voltage')
+plot(finalData$setTime, finalData$Sub_metering_1, type = 'l', xlab = '', ylab = 'Energy sub metering')
+lines(finalData$setTime, finalData$Sub_metering_2, type = 'l', col = 'red')
+lines(finalData$setTime, finalData$Sub_metering_3, type = 'l', col = 'blue')
 legend('topright', bty = 'n', legend = labels, lty = 1, col = columnLines)
-plot(finalData$setTime, finalData$Global_reactive_power, type = '1', col = 'blue', xlab = 'datetime', ylab = 'Global_reactive_power')
-
+plot(finalData$setTime, finalData$Global_reactive_power, type = 'l', col = 'blue', xlab = 'datetime', ylab = 'Global_reactive_power')
